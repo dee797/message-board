a4b21a85fd515c48a8dc2ad17bdc2f2b196279d8
dee797 <178827694+dee797@users.noreply.github.com>
2024-12-31 15:53:30 -0700

    Logic for finding message and sending it to message view

--- index.js
+++ index.js
@@ -9,27 +9,52 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 
+
 const messages = [
   {
+    id: 1,
     text: "Hi there!",
     user: "Amando",
     added: new Date()
   },
   {
+    id: 2,
     text: "Hello World!",
     user: "Charles",
     added: new Date()
   }
 ];
+
+async function getMessageById(messageId, messages) {
+  return messages.find(message => message.id == messageId);
+};
+
 app.locals.messages = messages;
-
-
 app.use(express.urlencoded({ extended: true }));
+
+
 
 app.use("/new", newMessageRouter);
 
 app.get('/', (req, res) => {
   res.render("index", { title: "Mini Messageboard", messages: app.locals.messages});
+});
+
+app.get("/:id", async (req, res) => {
+  try {
+    const message = await getMessageById(req.params.id, app.locals.messages);
+
+    if (!message) {
+      res.status(404).render("error");
+      return;
+    }
+
+    res.render("message", {message: message});
+
+  } catch (err) {
+    console.error(err);
+    res.status(505).send("Internal Server Error");
+  }
 });
 
 app.use((req, res, next) => {
