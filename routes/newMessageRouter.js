66daa8a6cb65dcb91cd8c001a2595da3ec89b80c
dee797 <178827694+dee797@users.noreply.github.com>
2024-12-31 12:45:55 -0700

    Use router.post() to handle post requests

--- routes/newMessageRouter.js
+++ routes/newMessageRouter.js
@@ -5,5 +5,9 @@ const newMessageRouter = Router();
 newMessageRouter.get("/", (req, res) => {
     res.render("form");
 });
+newMessageRouter.post("/", (req, res) => {
+    res.app.locals.messages.push({text: req.body.text, user: req.body.user, added: new Date()});
+    res.redirect("/");
+});
 
 module.exports = newMessageRouter;
