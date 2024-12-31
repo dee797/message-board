e28789f1c87f51924bc649db858dccb5c3028d75
dee797 <178827694+dee797@users.noreply.github.com>
2024-12-31 15:52:24 -0700

    Make object properties look nicer

--- routes/newMessageRouter.js
+++ routes/newMessageRouter.js
@@ -5,8 +5,14 @@ const newMessageRouter = Router();
 newMessageRouter.get("/", (req, res) => {
     res.render("form");
 });
+
 newMessageRouter.post("/", (req, res) => {
-    res.app.locals.messages.push({text: req.body.text, user: req.body.user, added: new Date()});
+    res.app.locals.messages.push({
+        id: res.app.locals.messages.length + 1, 
+        text: req.body.text, 
+        user: req.body.user, 
+        added: new Date()
+    });
     res.redirect("/");
 });
 
